56d9a2dfb5f3f850211736ff454c4dcaf5f94ad3
Antonio Brown <abrownjr12@outlook.com>
2024-09-24 19:33:46 -0500

    Saved Changes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function checkQualification() {
+   let  x = prompt();
+    x = Number(x);
+    const  incomeResult = document.getElementById("incomeResult")
+    if (x > 50000){
+        incomeResult.innerHTML = "You are qualified!";
+
+    } else{
+        incomeResult.innerHTML = "You are not qualified!";
+    }
+
+    
+}
+checkQualification();
